7b5d9c1b7d03d4920e7f2427546d74aac1436e2f
thegolbehead <x.movie.effects.x@gmail.com>
2022-06-16 21:38:06 +0300

    added home page

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome page"
+    return render_template("login_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
