455d6c74e541a0d8e256d194fdced249da8963c5
thegolbehead <x.movie.effects.x@gmail.com>
2022-06-18 13:54:37 +0300

    added css

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/')  
+def home():
+    return redirect("/login")
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
     return render_template("login_page.html")
+
+@app.route('/register')
+def register():
+    return render_template("register_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
