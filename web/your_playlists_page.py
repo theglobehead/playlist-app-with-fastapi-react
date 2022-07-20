018ae51d93663ab3645c9ae989db3e695a249749
Valters Vieško <76264011+theglobehead@users.noreply.github.com>
2022-07-20 17:22:17 +0300

    updated styles

--- web/your_playlists_page.py
+++ web/your_playlists_page.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 your_playlists_view = Blueprint("your-playlists", __name__)
 
 
 @your_playlists_view.route("/", methods=['GET', 'POST'])
 def your_playlists():
-    result = render_template("playlist_list.html")
+    user = session["user"]
+    result = render_template("playlist_list.html", user=user)
 
     return result
