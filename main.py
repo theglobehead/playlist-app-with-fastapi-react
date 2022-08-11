2b1a199c455d5537cc0b2d71634e5e2140d6253d
Valters Vieško <76264011+theglobehead@users.noreply.github.com>
2022-08-11 10:25:43 +0300

    made webiste choose the users' browsers' prefered language

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from web.site import site
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8f42a73054b1749h8f58848be5e6502c"
-app.config["BABEL_DEFAULT_LOCALE"] = "lv"
+app.config["BABEL_DEFAULT_LOCALE"] = "en"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["UPLOAD_FOLDER"] = "/static/uploads/"
@@ -24,6 +24,16 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 babel = Babel(app)
 
 psycopg2.extras.register_uuid()
+
+
+@babel.localeselector
+def get_locale():
+    locale = request.accept_languages.best_match(["lv", "en"])
+    if "locale" in session:
+        locale = session.get("locale")
+
+    session["locale"] = locale
+    return locale
 
 
 @app.route('/')
