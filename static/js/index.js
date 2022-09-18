56c1923521876f5a21b8c3563c8ba93963d7f1d4
Valters Vieško <76264011+theglobehead@users.noreply.github.com>
2022-09-18 12:56:13 +0300

    refactor: removed all views

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-function closeModal(modalId){
-    $(`#${ modalId }`).css("display", "none")
-}
-
-function openModal(modalId){
-    $(`#${ modalId }`).css("display", "flex")
-}
--- static/js/login.js
+++
@@ -1,7 +0,0 @@
-$(() => {
-    let username = Cookies.get('username')
-
-    if(username){
-        $("#username_input:first").val(username)
-    }
-})
